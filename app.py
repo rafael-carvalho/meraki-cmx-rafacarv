9da2738cbd970dd2518848e8e07fa2c6d2c41db8
rafacarv <rafacarv@cisco.com>
2017-04-12 13:25:11 -0300

    indenting

--- app.py
+++ app.py
@@ -25,8 +25,8 @@ def meraki():
         else:
             secret = "thisshouldbechanged"
             output = "Post Received. See logs for JSON"
-            #print (json.dumps(request.json, indent=4))
-            print (json.dumps(request.json))
+            print (json.dumps(request.json, indent=2))
+            #print (json.dumps(request.json))
 
     except:
         output = "Error when dealing with {}".format(request.method)
